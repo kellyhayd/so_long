b8f0b4d5666b9edd076e312d8d8295c319975e5c
Kelly Hayd <hayd.kelly@gmail.com>
2024-01-24 16:35:20 -0300

    map validations working! 'check_format' hates norminette

--- src/maps.c
+++ src/maps.c
@@ -5,69 +5,87 @@
 #include <stdio.h>
 #include <aio.h>
 
-int32_t	validate_map(int fd)
+int32_t	check_components(int32_t start, int32_t exit, int32_t collectibles)
+{
+	if (start > 1 || start == 0)
+		return (0); //erro de posição start
+	if (exit > 1 || exit == 0)
+		return (0); //erro de saída
+	if (collectibles == 0)
+		return (0); //erro de colecionáveis
+	return (1);
+}
+
+char	*read_line(int fd, int32_t fst_line_size)
 {
 	char	*line;
-	int		i;
-	int32_t	count_p = 0;
-	int32_t	count_e = 0;
-	int32_t	count_c = 0;
-	int32_t	fst_line_size = 0;
-	int32_t	lst_line_size = 0;
+	int32_t	lst_line_size;
 
-	line = get_next_line(fd); //leitura da primeira linha que precisa ser parede
-	fst_line_size = ft_strlen(line); //primeira linha define tamanho do mapa
-	printf("%s\n", line); //EXCLUIR *****
-	while (*line != '\n')
-	{
-		if (!(ft_strchr(line, '1')))
-		{
-			printf("primeira linha deu erro\n"); //EXCLUIR *****
-			return (0); //erro de parede 
-		}
-		else
-			*line++;
-	}
-	printf("tamanho da PRIMEIRA linha = %d\n", fst_line_size); //EXCLUIR *****
+	lst_line_size = 0;
+	line = get_next_line(fd);
+	if (line == NULL)
+		return (NULL);
+	if (!ft_strchr(line, '\n'))
+		lst_line_size = ft_strlen(line) + 1;
+	else
+		lst_line_size = ft_strlen(line);
+	if (lst_line_size != fst_line_size) //todas as linhas precisam ter o mesmo tamanho
+		return (NULL); //erro de formato
+	return (line);
+}
+
+int32_t	check_format(int fd, int32_t fst_line_size) //MAIS DE 25 LINHAS, CHATONETTE DO CAPETA!!!!
+{
+	int		i;
+	char	*line;
+	int32_t	count_p;
+	int32_t	count_e;
+	int32_t	count_c;
+
+	count_p = 0;
+	count_e = 0;
+	count_c = 0;
 	while (fd)
 	{
-		line = get_next_line(fd);
+		line = read_line(fd, fst_line_size);
 		if (line == NULL)
 			break;
-		if (!ft_strchr(line, '\n'))
-			lst_line_size = ft_strlen(line) + 1;
-		else
-			lst_line_size = ft_strlen(line);
-		printf("%s\n", line); //EXCLUIR *****
-		printf("tamanho da linha atual = %d\n", lst_line_size); //EXCLUIR *****
-		if (lst_line_size != fst_line_size) //todas as linhas precisam ter o mesmo tamanho
-			return (printf("mapa torto")); //erro de formato
 		i = 0;
 		while (line[i])
 		{
 			if (line[0] != '1' || line[ft_strlen(line) - 2] != '1')
-			{
-				printf("%c\n", line[i]); //EXCLUIR *****
 				return (0); //erro de parede
-			}
 			if (line[i] == 'P')
 				count_p++;
-			else if (line[i] == 'C')
-				count_c++;
 			else if (line[i] == 'E')
 				count_e++;
+			else if (line[i] == 'C')
+				count_c++;
 			i++;
 		}
 	}
-	printf("START = %d\n", count_p); //EXCLUIR *****
-	printf("EXIT = %d\n", count_e); //EXCLUIR *****
-	printf("COLLECTIBLES = %d\n", count_c); //EXCLUIR *****
-	if (count_p > 1 || count_p <= 0)
-		return (0); //erro de posição start
-	if (count_e > 1 || count_e <= 0)
-		return (0); //erro de saída
-	if (count_c <= 0)
-		return (0); //erro de colecionáveis
+	if (!check_components(count_p, count_e, count_c))
+		return (0);
+	return (1);
+}
+
+int32_t	validate_map(int fd)
+{
+	char	*line;
+	int		i;
+	int32_t	fst_line_size = 0;
+
+	line = get_next_line(fd); //leitura da primeira linha que precisa ser parede
+	fst_line_size = ft_strlen(line); //primeira linha define tamanho do mapa
+	while (*line != '\n')
+	{
+		if (!(ft_strchr(line, '1')))
+			return (0); //erro de parede 
+		else
+			*line++;
+	}
+	if (!check_format(fd, fst_line_size))
+		return (0);
 	return (1); //sucesso! mapa aprovado!
 }
 
@@ -80,5 +98,6 @@ int main()
 		printf("sucesso na missão!\n");
 	else
 		printf("deu ruim no mapa\n");
+	close(fd);
 	return (0);
 }
