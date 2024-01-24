796d0542a5f40fdab3b3fbe069dc651fbc2f1299
Kelly Hayd <hayd.kelly@gmail.com>
2024-01-24 14:23:41 -0300

    map validation working! 1 start, 1 exit, -1 collectible, surrounded by walls, rectangular

--- src/maps.c
+++ src/maps.c
@@ -13,30 +13,43 @@ int32_t	validate_map(int fd)
 	int32_t	count_e = 0;
 	int32_t	count_c = 0;
 	int32_t	fst_line_size = 0;
+	int32_t	lst_line_size = 0;
 
 	line = get_next_line(fd); //leitura da primeira linha que precisa ser parede
-	printf("%s\n", line);
-	while (line)
+	fst_line_size = ft_strlen(line); //primeira linha define tamanho do mapa
+	printf("%s\n", line); //EXCLUIR *****
+	while (*line != '\n')
 	{
-		if (!ft_strchr(line, '1'))
+		if (!(ft_strchr(line, '1')))
 		{
-			printf("primeira linha deu erro\n");
-			return (0); //erro de parede
+			printf("primeira linha deu erro\n"); //EXCLUIR *****
+			return (0); //erro de parede 
 		}
 		else
 			*line++;
 	}
-	fst_line_size = ft_strlen(line); //primeira linha define tamanho do mapa
+	printf("tamanho da PRIMEIRA linha = %d\n", fst_line_size); //EXCLUIR *****
 	while (fd)
 	{
 		line = get_next_line(fd);
-		if (ft_strlen(line) != fst_line_size) //todas as linhas precisam ter o mesmo tamanho
-			return (0); //erro de formato
+		if (line == NULL)
+			break;
+		if (!ft_strchr(line, '\n'))
+			lst_line_size = ft_strlen(line) + 1;
+		else
+			lst_line_size = ft_strlen(line);
+		printf("%s\n", line); //EXCLUIR *****
+		printf("tamanho da linha atual = %d\n", lst_line_size); //EXCLUIR *****
+		if (lst_line_size != fst_line_size) //todas as linhas precisam ter o mesmo tamanho
+			return (printf("mapa torto")); //erro de formato
 		i = 0;
 		while (line[i])
 		{
-			if (line[0] != '1' || line[ft_strlen(line) - 1] != '1')
+			if (line[0] != '1' || line[ft_strlen(line) - 2] != '1')
+			{
+				printf("%c\n", line[i]); //EXCLUIR *****
 				return (0); //erro de parede
+			}
 			if (line[i] == 'P')
 				count_p++;
 			else if (line[i] == 'C')
@@ -46,6 +59,9 @@ int32_t	validate_map(int fd)
 			i++;
 		}
 	}
+	printf("START = %d\n", count_p); //EXCLUIR *****
+	printf("EXIT = %d\n", count_e); //EXCLUIR *****
+	printf("COLLECTIBLES = %d\n", count_c); //EXCLUIR *****
 	if (count_p > 1 || count_p <= 0)
 		return (0); //erro de posição start
 	if (count_e > 1 || count_e <= 0)
@@ -61,7 +77,7 @@ int main()
 
 	fd = open("./maps/map_01.ber", O_RDONLY);
 	if (validate_map(fd) == 1)
-		printf("sucesso na missão!");
+		printf("sucesso na missão!\n");
 	else
 		printf("deu ruim no mapa\n");
 	return (0);
