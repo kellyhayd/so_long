be9e13823d38c0b3f6b82dcfd09d99b0f80f2751
Kelly Hayd <hayd.kelly@gmail.com>
2024-01-24 16:45:47 -0300

    header so_long

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 13:10:08 by krocha-h          #+#    #+#             */
-/*   Updated: 2024/01/17 13:55:40 by krocha-h         ###   ########.fr       */
+/*   Updatedt_: 2024/01/17 13:55:40 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #	include <stdio.h>
 #	include <unistd.h>
 #	include <string.h>
-#	include "MLX42/MLX42.h"
+// #	include "MLX42/MLX42.h"
 
 
 #	define WIDTH 256
@@ -32,13 +32,20 @@
  * @param height The height of the window.
  * @param delta_time The time difference between the previous frame and the current frame.
  */
-// typedef struct	mlx
-// {
-// 	void	*window;
-// 	void	*context;
-// 	int32_t	width;
-// 	int32_t	height;
-// 	double	delta_time;
-// }	mlx_t;
+typedef struct	s_mlx
+{
+	void	*window;
+	void	*context;
+	int32_t	width;
+	int32_t	height;
+	double	delta_time;
+}	t_mlx;
+
+typedef struct s_fmt_map
+{
+	int32_t	start;
+	int32_t	exit;
+	int32_t	collectibles;
+}	t_fmt_map;
 
 #endif
