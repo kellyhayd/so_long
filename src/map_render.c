fb7c0320b37dc94b95d05a758bd67cd9bca78d5e
Kelly Hayd <hayd.kelly@gmail.com>
2024-01-26 15:36:53 -0300

    change name of define_tiles to components_position

--- src/map_render.c
+++ src/map_render.c
@@ -4,19 +4,40 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-void	define_titles(mlx_t *mlx, mlx_image_t* img, t_map *map_info)
+void	define_imgs(t_game *game, mlx_t *mlx)
+{
+	mlx_texture_t*	texture;
+	mlx_image_t*	img;
+
+	texture = mlx_load_png("images/tile.png");
+	img = mlx_texture_to_image(mlx, texture);
+	game->tile = img;
+	texture = mlx_load_png("images/cats/cat_walking_01.png");
+	img = mlx_texture_to_image(mlx, texture);
+	game->hero = img;
+	// texture = mlx_load_png("images/enemy.png");
+	// img = mlx_texture_to_image(mlx, texture);
+	// game->enemy = img;
+	// texture = mlx_load_png("images/star.png");
+	// img = mlx_texture_to_image(mlx, texture);
+	// game->star = img;
+}
+
+void	components_position(mlx_t *mlx, t_game *game)
 {
 	int32_t	i;
 	int32_t	j;
 
 	i = 0;
-	while (i < map_info->height)
+	while (i < game->map->height)
 	{
 		j = 0;
-		while (j < map_info->width)
+		while (j < game->map->width)
 		{
-			if (map_info->matrix[i][j] == '1')
-				mlx_image_to_window(mlx, img, j * 50, i * 50);
+			if (game->map->matrix[i][j] == '1')
+				mlx_image_to_window(mlx, game->tile, j * 64, i * 64);
+			else if (game->map->matrix[i][j] == 'P')
+				mlx_image_to_window(mlx, game->hero, j * 64, i * 64);
 			j++;
 		}
 		i++;
