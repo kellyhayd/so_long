04072d43455724cff1f43bc7c9c5dfe84146fd80
Kelly Hayd <hayd.kelly@gmail.com>
2024-02-12 21:53:48 -0300

    Simplify error check in main_bonus.c

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -17,14 +17,11 @@ int32_t	main(int argc, char **argv)
 	int32_t	fd;
 	t_game	*game;
 
-	if (argc == 2)
-	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd < 0)
-			return (ft_printf("Error opening file"), EXIT_FAILURE);
-	}
-	else
+	if (argc != 2)
 		return (ft_putstr_fd(MSG_ARGS, 2), EXIT_FAILURE);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		return (ft_printf("Error opening file"), EXIT_FAILURE);
 	game = (t_game *)ft_calloc(sizeof(t_game), 1);
 	if (!game)
 		return (EXIT_FAILURE);
